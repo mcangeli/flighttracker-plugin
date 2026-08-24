e7be788947630607b6db205519a9597950c52175
mcangeli <mcangeli@gmail.com>
2026-08-24 13:30:10 -0400

    Delete flighttracker directory

--- flighttracker/__init__.py
+++
@@ -1,8 +0,0 @@
-from .config import FlightTrackerConfig
-from .data import FlightTrackerData
-from .renderer import FlightTrackerRenderer
-
-
-def load():
-    """Bullpen entrypoint hook."""
-    return FlightTrackerConfig, FlightTrackerData, FlightTrackerRenderer
