c4fde2e143e2469cede962975162b70f690f2f03
mcangeli <mcangeli@gmail.com>
2026-08-24 14:58:54 -0400

    Update setup.py

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="flighttracker-plugin",
-    version="0.1.4",
+    version="0.1.5",
     description="Overhead aircraft tracking plugin for MLB-LED-Scoreboard",
     packages=find_packages(),
     install_requires=[
